f60d4bbfa1396fb54593d39363a510602845ddd6
tisto <imtisto@gmail.com>
2024-05-18 02:14:04 +0700

    Initial commit

---
+++ app/printer/src/main/java/com/zenenta/printer/AppExt.kt
@@ -0,0 +1,5 @@
+package com.zenenta.printer
+
+fun showPrinter(message:String){
+    println(message)
+}
---
+++ loglib/src/main/java/com/dewakoding/loglib/LogLib.kt
@@ -0,0 +1,7 @@
+package com.dewakoding.loglib
+
+import android.util.Log
+
+fun logD(message: String) {
+    Log.d("zenenta", message)
+}
