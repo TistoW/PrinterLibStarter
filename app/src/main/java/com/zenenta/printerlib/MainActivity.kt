6efca9e727320622962c665fd1108749c87d4fb4
tisto <imtisto@gmail.com>
2024-05-18 02:28:45 +0700

    - feat:update sdk

--- app/src/main/java/com/zenenta/printerlib/MainActivity.kt
+++ app/src/main/java/com/zenenta/printerlib/MainActivity.kt
@@ -2,15 +2,10 @@ package com.zenenta.printerlib
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import com.zenenta.printer.showPrinter
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        Log.d("TAG", "onCreate: ")
-        showPrinter("Hello from PrinterLib")
     }
 }
